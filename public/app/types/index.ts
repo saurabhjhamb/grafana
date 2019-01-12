28b16ee57c1f788edcca47dfa6757dc3af53aee0
David Kaltschmidt <david.kaltschmidt@gmail.com>
2019-01-12 23:28:51 +0100

    Move types to types/explore

--- public/app/types/index.ts
+++ public/app/types/index.ts
@@ -20,7 +20,7 @@ import {
 } from './appNotifications';
 import { DashboardSearchHit } from './search';
 import { ValidationEvents, ValidationRule } from './form';
-import { ExploreState } from 'app/features/explore/state/reducers';
+import { ExploreState } from './explore';
 export {
   Team,
   TeamsState,
